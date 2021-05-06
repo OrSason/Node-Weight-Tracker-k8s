ee339f7235a2d0c557590b25add31fccfb6be2db
OrSason <81312487+OrSason@users.noreply.github.com>
2021-05-06 11:20:47 +0300

    Update configEnv.sh

--- configEnv.sh
+++ configEnv.sh
@@ -13,9 +13,9 @@ NODE_ENV=development
 HOST_URL=http://${ip}:8080
 COOKIE_ENCRYPT_PWD=superAwesomCOOKIE_ENCRYPT_PWD=superAwesomePasswordStringThatIsAtLeast32CharactersLong! ePasswordStringThatIsAtLeast32CharactersLong!
 # Okta configuration
-OKTA_ORG_URL=https://${domain}
-OKTA_CLIENT_ID=${clientid}
-OKTA_CLIENT_SECRET=${secret}
+OKTA_ORG_URL=https://$1
+OKTA_CLIENT_ID=$2
+OKTA_CLIENT_SECRET=$3
 # Postgres configuration
 PGHOST=11.0.2.10
 PGUSERNAME=postgres
