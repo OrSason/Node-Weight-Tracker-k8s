ab7fd381a74230e585320f904b78ae5566a71ad2
OrSason <81312487+OrSason@users.noreply.github.com>
2021-05-06 11:27:56 +0300

    Update configEnv.sh

--- configEnv.sh
+++ configEnv.sh
@@ -1,13 +1,12 @@
 
 #!/bin/bash
 
-ip=$(curl https://ipinfo.io/ip)
 
 echo "# Host configuration
 PORT=8080
 HOST=0.0.0.0
 NODE_ENV=development
-HOST_URL=http://${ip}:8080
+HOST_URL=http://40.67.252.238:8080
 COOKIE_ENCRYPT_PWD=superAwesomCOOKIE_ENCRYPT_PWD=superAwesomePasswordStringThatIsAtLeast32CharactersLong! ePasswordStringThatIsAtLeast32CharactersLong!
 # Okta configuration
 OKTA_ORG_URL=https://$1
